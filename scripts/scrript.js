268b35fb51213fd259e404928b4510ed37b016c9
Steven Rivera <ghostszmusic@gmail.com>
2025-07-03 12:32:27 -0500

    Update scrript.js

--- scripts/scrript.js
+++ scripts/scrript.js
@@ -1,8 +1,9 @@
 // JavaScript Document
 
-var URI_weather = 'https://ghostszmusic.com/wp-json/_restroute/amp-OW';
+var URI_weather = 'https://api.openweathermap.org/data/3.0/onecall?lat=37.061120&lon=-93.309500&exclude=minutely,hourly,alerts&units=imperial&appid=3c890e2eb127beb3199800aea092bd52';
 var current = [];
 var forecast = [];
+var currentTime = new Date();
 
 function addDays(date, days) {
     var result = new Date(date);
@@ -12,21 +13,16 @@ function addDays(date, days) {
 
 $.ajax({
   url: URI_weather,
-  dataType: "json",
-	timeout: 0,
-	headers: {
-	  "Authorization": "Basic YXBwRGF0YTpORVdIIGVFYncgWDR1SSBIUVVPIFgyblogMWdsSg==",
-	},
+  dataType: "jsonp",
   success: function (data) {
-
+    
   current = data.current.weather[0];
-  forecast[0] = data.daily[1].weather[0];
-  forecast[1] = data.daily[2].weather[0];
-  forecast[2] = data.daily[3].weather[0];
-  var currentTime = new Date(data.current.dt * 1000);
+  forecast[0] = data.daily[0].weather[0];
+  forecast[1] = data.daily[1].weather[0];
+  forecast[2] = data.daily[2].weather[0];
+	  
 
   switch(current.main){
-
 	  case 'Clear':
 		 $('.ghs_current_weather').append('<div><p class="ghs_time">'+currentTime.toLocaleDateString()+'</p><i class="fas fa-sun"></i><h2 class="weather_status">'+current.description+'</h2><p class="ghs_temp">'+Math.round(data.current.temp)+'&#8457;</p></div>');
 		  break;
@@ -58,9 +54,9 @@ $.ajax({
 	  
 	  for(var i = 0; i<=2; i++){
 		  
-		  var dtae = new Date(data.daily[i+1].dt * 1000).toLocaleDateString();
+		  var dtae = new Date(addDays(currentTime, i+1)).toLocaleDateString();
 		  
-		switch(forecast[i].main){
+		  	switch(forecast[i].main){
 	  	case 'Clear':
 		 $('.ghs_forecast_weather').append('<li class="ghs_day_'+i+'"><p class="ghs_time">'+dtae+'</p><i class="fas fa-sun"></i><h2 class="weather_status">'+forecast[i].description+'</h2><div class="ghs_min_max_temps"><p class="ghs_temp_daily">'+Math.round(data.daily[i].temp.min)+'&#8457;</p><p class="ghs_temp_daily">'+Math.round(data.daily[i].temp.max)+'&#8457;</p></div></li>');
 		  break;
